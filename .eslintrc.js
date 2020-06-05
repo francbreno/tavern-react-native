5965ee809cce7fb61288172cfbf64bb8a341c4f2
Breno Soares <francbreno@gmail.com>
2020-06-05 00:18:31 -0300

    Configured ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,41 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+    'env': {
+        'browser': true,
+        'es6': true,
+        'jest': true,
+    },
+    'extends': [
+        'eslint:recommended',
+        'plugin:react/recommended'
+    ],
+    'globals': {
+        'Atomics': 'readonly',
+        'SharedArrayBuffer': 'readonly'
+    },
+    'parserOptions': {
+        'ecmaFeatures': {
+            'jsx': true
+        },
+        'ecmaVersion': 2018,
+        'sourceType': 'module'
+    },
+    'plugins': [
+        'react'
+    ],
+    'rules': {
+        'indent': [
+            'error',
+            2
+        ],
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'quotes': ['error', 'single', { avoidEscape: true }],
+        'no-empty-function': 'off',
+        'semi': [
+            'error',
+            'always'
+        ]
+    }
 };
