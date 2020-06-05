e069131c35dcc35a65f6a24dcf81dbf4d1b880f7
Breno Soares <francbreno@gmail.com>
2020-06-05 00:35:54 -0300

    Fixed react eslint plugin config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,40 +1,33 @@
 module.exports = {
-    'env': {
-        'browser': true,
-        'es6': true,
-        'jest': true,
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    'extends': [
-        'eslint:recommended',
-        'plugin:react/recommended'
-    ],
-    'globals': {
-        'Atomics': 'readonly',
-        'SharedArrayBuffer': 'readonly'
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-hooks', 'prettier'],
+  rules: {
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single', { avoidEscape: true }],
+    'no-empty-function': 'off',
+    'prettier/prettier': 'error',
+    semi: ['error', 'always'],
+  },
+  settings: {
+    react: {
+      version: 'detect',
     },
-    'parserOptions': {
-        'ecmaFeatures': {
-            'jsx': true
-        },
-        'ecmaVersion': 2018,
-        'sourceType': 'module'
-    },
-    'plugins': ['react', 'react-hooks', 'prettier'],
-    'rules': {
-        'indent': [
-            'error',
-            2
-        ],
-        'linebreak-style': [
-            'error',
-            'unix'
-        ],
-        'quotes': ['error', 'single', { avoidEscape: true }],
-        'no-empty-function': 'off',
-        'prettier/prettier': 'error',
-        'semi': [
-            'error',
-            'always'
-        ]
-    }
+  },
 };
